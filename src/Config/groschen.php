8325623cc039f7f0379acfbd77b518fe1dcc3b92
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2017-09-27 10:21:50 +0300

    Added config template

--- src/Config/groschen.php
+++ src/Config/groschen.php
@@ -1,1 +1,11 @@
-groschen.php
+<?php
+
+return [
+    'schilling' => [
+        'hostname' => env('SCHILLING_WEB_SERVICES_HOSTNAME', 'prod-schilling.domain.com'),
+        'port' => env('SCHILLING_WEB_SERVICES_PORT', '8888'),
+        'username' => env('SCHILLING_WEB_SERVICES_USERNAME', 'webserviceuser'),
+        'password' => env('SCHILLING_WEB_SERVICES_PASSWORD', 'foobar'),
+        'company' => env('SCHILLING_WEB_SERVICES_COMPANY', '1001'),
+    ],
+];
