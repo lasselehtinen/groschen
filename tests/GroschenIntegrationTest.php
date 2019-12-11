77dc819ee05fc764a1933eb21939d21a3b1d9606
Lasse Lehtinen <lasse.lehtinen@wsoy.fi>
2019-12-11 12:40:05 +0200

    Style fix

--- tests/GroschenIntegrationTest.php
+++ tests/GroschenIntegrationTest.php
@@ -697,7 +697,8 @@ class GroschenIntegrationTest extends TestCase
      * Test getting the products internal category
      * @return void
      */
-    public function testGettingInternalCategory() {
+    public function testGettingInternalCategory()
+    {
         $this->assertFalse($this->groschen->getSubjects()->contains('SubjectSchemeIdentifier', '24'));
 
         $groschen = new Groschen('9789520418564');
